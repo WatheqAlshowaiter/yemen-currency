7f633d985591061bcf24398c89f4c53783e2daf3
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2025-10-15 15:20:43 +0300

    feat(docker): add laravel sail configuration
    - Added docker-compose.yml with PHP 8.4 container
    - Configured app to run on port 8080 via APP_PORT
    - Updated storage and bootstrap permissions for Docker
    - Removed MySQL service to use SQLite database

--- routes/console.php
+++ routes/console.php
@@ -8,13 +8,13 @@ Schedule::command('currency:fetch --today')
     ->onFailure(function () {
         // TODO: set the correct sender email
         // TODO: set the correct recipient email
-         
+
         // \Illuminate\Support\Facades\Mail::raw(
         //     "Currency fetching failed!\n\nCheck the page: https://boqash.com/price-currency\nAnd review the logs for more details.",
         //     function (\Illuminate\Mail\Message $message) {
         //         $message
-        //             ->from(config('mail.from.address')) 
-        //             ->to(config('mail.admin.address'))   
+        //             ->from(config('mail.from.address'))
+        //             ->to(config('mail.admin.address'))
         //             ->subject('Currency Fetching Failed!');
         //     }
         // );
